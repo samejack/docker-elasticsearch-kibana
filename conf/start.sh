66c56a89b90c30a96af7e6b1ce248667de5a4245
SJ Chou <sj@toright.com>
2017-11-21 10:09:42 +0800

    Update start.sh
    set ext remote dictionary by environment

--- conf/start.sh
+++ conf/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# setup remote dictionary
+if [ "${REMOVE_EXT_DICT}" != "" ]; then
+  echo "Set remove ext dict: ${REMOVE_EXT_DICT}"
+  sed -i -e "s/<\!-- <entry key=\"remote_ext_dict\">words_location<\/entry> -->/<entry key=\"remote_ext_dict\">${REMOVE_EXT_DICT//\//\\/}<\/entry>/g" /usr/share/elasticsearch/plugins/ik/config/IKAnalyzer.cfg.xml
+fi
+if [ "${REMOVE_EXT_STOPWORDS}" != "" ]; then
+  echo "Set remove ext stop words: ${REMOVE_EXT_STOPWORDS}"
+  sed -i -e "s/<\!-- <entry key=\"remote_ext_stopwords\">words_location<\/entry> -->/<entry key=\"remote_ext_stopwords\">${REMOVE_EXT_STOPWORDS//\//\\/}<\/entry>/g" /usr/share/elasticsearch/plugins/ik/config/IKAnalyzer.cfg.xml
+fi
+
 service elasticsearch start
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
